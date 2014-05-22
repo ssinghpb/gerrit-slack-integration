d91030a431a56b7c3b8ead8dffa1634ccf743c68
Aleksey Didik <aleksey.didik@maxifier.com>
2014-05-22 19:14:54 -0400

    change-merge v1

--- change-merged.py
+++ change-merged.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2012, JT Olds <hello@jtolds.com>
+# Copyright (c) 2014, Aleksey Didik <aleksey.didik@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,26 +22,38 @@
 #
 
 """
-    Gerrit Webhook
+    Gerrit Slack Integration Webhook
 """
 
-__author__ = "JT Olds"
-__author_email__ = "hello@jtolds.com"
+__author__ = "Aleksey Didik"
+__author_email__ = "aleksey.didik@gmail.com"
 
-GERRIT_URL = "http://gerrit.example.com/"
-GERRIT_NAME = "Gerrit Code Review"
-GERRIT_DESCRIPTION = ""
-GERRIT_OWNER_NAME = "Admin"
-GERRIT_OWNER_EMAIL = "admin@example.com"
-GERRIT_PORT = 29418
-GERRIT_SERVER = "localhost"
-WEBHOOK_URL = "http://webhook.example.com/new-commit"
+# Configuration section
+
+GERRIT_PORT = 22
+GERRIT_SERVER = "aleksey.didik@git.maxifier.com"
+WEBHOOK_URL = "https://maxifier.slack.com/services/hooks/incoming-webhook?token=Vjweuw0Ld7GszR3OqRdEXNpU"
+
+# Mapping #channel to Gerrit projects.
+# One channel can be mapped on several projects.
+# Use regexp for 
+# E.g. to map all projects to general and #web channel 
+# to projects web-project and web-design set:
+#     {"gerrit": [".*"], "web": ["web-project", "web-design"]}
+CHANNEL_MAPPING = {"#general": [".*"]}
+
+# emoji icon to be used in a message.
+# set value "" to use Slack defined icon
+ICON_EMOJI = ":white_check_mark:"
+
+# End of coniguration section
 
 import json
 import time
 import urllib
 import urllib2
 import subprocess
+import re
 from optparse import OptionParser
 
 
@@ -51,33 +63,41 @@ def getCommitInfo(commit_hash):
                 ["ssh", "-p", str(GERRIT_PORT), GERRIT_SERVER, "gerrit",
                  "query", "--commit-message", "--format", "json",
                  commit_hash]).split("\n")[0])
-        return (result["commitMessage"], result["lastUpdated"],
-                result["owner"]["name"], result["owner"]["email"])
+        return (result["commitMessage"], result["owner"]["name"])
     except Exception, e:
         return ("Failed getting commit message, %s: %s" % (
                     e.__class__.__name__, e),
                 int(time.time()), "unknown user", "unknown@example.com")
 
 
-def webhook(commit_hash, branch, name, email, change_url, message, timestamp):
+def webhook(channel, project, branch, name, change_url, message, submitter):  
+
     data = {
-        "before": "",
-        "after": commit_hash,
-        "ref": "refs/heads/%s" % branch,
-        "repository": {
-            "url": GERRIT_URL,
-            "name": GERRIT_NAME,
-            "description": GERRIT_DESCRIPTION,
-            "owner": {
-                "name": GERRIT_OWNER_NAME,
-                "email": GERRIT_OWNER_EMAIL}},
-        "commits": [{"id": commit_hash,
-                     "author": {"name": name, "email": email},
-                     "url": change_url,
-                     "message": message,
-                     "timestamp": timestamp}]}
-    urllib2.urlopen(WEBHOOK_URL,
-                    urllib.urlencode({"payload": json.dumps(data)})).read()
+        "channel" : channel,
+        "pretext": "@{0} has merged change <{1}|{2}>".format(submitter, change_url, message.splitlines()[0]),
+        "color": "#00D000",
+        "fields": [
+            {
+                "title": "Author",
+                "value": name,
+                "short": "true"
+            },
+            {
+                "title": "Project",
+                "value": project,                
+            },
+            {
+                "title": "Branch",
+                "value": branch,                
+            },
+        ],
+        "link_names": 1,        
+        "fallback" : "@{0} has merged change {1}\n Branch: {2}\n Link: {3}".format(submitter, message, branch, change_url)
+    }
+    if ICON_EMOJI != "":
+        data["icon_emoji"] = ICON_EMOJI
+
+    urllib2.urlopen(WEBHOOK_URL, urllib.urlencode({"payload": json.dumps(data)})).read()
 
 
 def main():
@@ -89,13 +109,14 @@ def main():
     parser.add_option("--topic", help="Topic name")
     parser.add_option("--submitter", help="Submitter")
     parser.add_option("--commit", help="Git commit hash")
-
     options, args = parser.parse_args()
-    message, timestamp, name, email = getCommitInfo(options.commit)
-    timestamp = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(timestamp))
-    webhook(options.commit, options.branch, name, email, options.change_url,
-            message, timestamp)
 
+    message,name = getCommitInfo(options.commit)    
+
+    for channel in CHANNEL_MAPPING: 
+        for project_re in CHANNEL_MAPPING[channel]:
+            if  re.compile(project_re).match(options.project):
+                    webhook(channel, options.project, options.branch, name, options.change_url, message, options.submitter)
 
 if __name__ == "__main__":
     main()
